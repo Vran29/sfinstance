124345d3bb02e50389850346253fbfdf85d3a535
Vran29 <121425298+Vran29@users.noreply.github.com>
2024-11-04 21:54:22 +0100

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,22 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://superlative-marigold-9f4e59.netlify.app/",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTgwZTRmM2FhNjVlY2JhNDZlMzVjNjhjZmY1YTA0YSIsIm5iZiI6MTczMDc1MzAwOC4xNjMxNTcsInN1YiI6IjY3MjkzMTc5Mzk0MGMxMjAyZmY3ZGU3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PL89_8Er_OlNNP0NHqteccCR23PQrFz_ZOjcQg8UCrg",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: true,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: "https://backend.sudo-flix.lol",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+
+  VITE_ALLOW_AUTOPLAY: true,
+};
